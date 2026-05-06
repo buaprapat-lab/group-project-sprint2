d89f9d9507d34ceceb427c4064ce8860c06a9683
pipatpongzosp-lgtm <pipatpongzosp@gmail.com>
2026-05-06 15:53:01 +0700

    add Footerbar

---
+++ src/component/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PickupBar from './customer/PickupBar';
+import FooterLinks from './FooterLinks';
+
+export default function Footer({ t }) {
+  return (
+    <footer className="bg-black text-white w-full">
+
+      <FooterLinks t={t} />
+    </footer>
+  );
+}
