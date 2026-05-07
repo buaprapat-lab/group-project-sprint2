1407755ea79b0896b53045b4f04e6b8e996bc488
buaprapat-lab <buaprapat@gmail.com>
2026-05-07 11:16:19 +0700

    added sfc-design-system.md

--- src/component/customer/Hero.jsx
+++ src/component/customer/Hero.jsx
@@ -30,7 +30,7 @@ export default function Hero() {
       {/* ─── Image Container ─── */}
       <div
         // เอาคลาส group ออกไปแล้ว เพื่อไม่ให้มันส่งสัญญาณ Hover ไปที่รูป
-        className={`h-100 md:h-[70vh] bg-[#1c1c1c] relative shadow-[0_20px_50px_rgba(0,0,0,0.5)] border-y border-white/5 overflow-hidden transition-all duration-1200 ease-[cubic-bezier(0.25,1,0.5,1)] ${
+        className={`h-100 md:h-[70vh] bg-[#1c1c1c] relative shadow-[0_20px_50px_rgba(0,0,0,0.5)] border-y border-white/5 overflow-hidden transition-all duration-1200gi ease-[cubic-bezier(0.25,1,0.5,1)] ${
           isExpanded
             ? "w-full max-w-full rounded-none" // ตอนขยาย: กางเต็ม 100% ขอบเหลี่ยม นิ่งๆ
             : "w-[90%] md:w-[85%] max-w-350 rounded-4xl md:rounded-[3rem]" // ตอนโหลดครั้งแรก
